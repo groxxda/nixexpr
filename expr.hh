4bd0c025d1d8a3bb2978c3f18f3b9b8ff3eee622
Alexander Ried <ried@mytum.de>
2016-10-26 00:54:55 +0200

    add keywords

--- expr.hh
+++ expr.hh
@@ -35,6 +35,36 @@ namespace ast {
 } // namespace ast
 
 namespace parser {
+namespace keyword {
+    struct str_if      : pegtl::string<'i', 'f'>                          {};
+    struct str_then    : pegtl::string<'t', 'h', 'e', 'n'>                {};
+    struct str_else    : pegtl::string<'e', 'l', 's', 'e'>                {};
+    struct str_assert  : pegtl::string<'a', 's', 's', 'e', 'r', 't'>      {};
+    struct str_with    : pegtl::string<'w', 'i', 't', 'h'>                {};
+    struct str_let     : pegtl::string<'l', 'e', 't'>                     {};
+    struct str_in      : pegtl::string<'i', 'n'>                          {};
+    struct str_rec     : pegtl::string<'r', 'e', 'c'>                     {};
+    struct str_inherit : pegtl::string<'i', 'n', 'h', 'e', 'r', 'i', 't'> {};
+    struct str_or      : pegtl::string<'o', 'r'>                          {};
+
+    struct str_any     : pegtl::sor<str_if, str_then, str_else, str_assert, str_with, str_let, str_in, str_rec, str_inherit, str_or> {};
+
+    template<typename Key>
+    struct key         : pegtl::seq<Key, pegtl::not_at<pegtl::identifier_other>> {};
+
+    struct key_then    : key<str_then>    {};
+    struct key_else    : key<str_else>    {};
+    struct key_assert  : key<str_assert>  {};
+    struct key_with    : key<str_with>    {};
+    struct key_let     : key<str_let>     {};
+    struct key_in      : key<str_in>      {};
+    struct key_rec     : key<str_rec>     {};
+    struct key_inherit : key<str_inherit> {};
+    struct key_or      : key<str_or>      {};
+
+    struct any         : key<str_any>     {};
+
+} // namespace keyword
 
     struct grammar : pegtl::must<pegtl::eof> {};
 
@@ -42,3 +72,9 @@ namespace parser {
 } // namespace parser
 
 } // namespace nix
+
+
+
+
+
+
